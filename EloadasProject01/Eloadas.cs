b9774e57cddb9b241853ba966d3b495c6f9e71a0
Garrus20 <garruswasab@gmail.com>
2020-02-21 12:39:40 +0100

    Add project files.

---
+++ EloadasProject01/Eloadas.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloadasProject01
+{
+    public class Eloadas
+    {
+        private bool[,] foglalasok;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            foglalasok = new bool[sorokSzama, helyekSzama];
+        }
+
+        public bool Lefoglal()
+        {
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        foglalasok[i, j] = true;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public int GetSzabadHelyek()
+        {
+            int seged = 0;
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        seged++;
+                    }
+                }
+            }
+            return seged;
+        }
+
+        public bool Teli()
+        {
+            int seged = 0;
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        seged++;
+                    }
+                }
+            }
+            if (seged == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Foglalt(int sor, int szek)
+        {
+            if (foglalasok[sor, szek] == true)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ EloadasProject01/Tests/EloadasokTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloadasProject01.EloadasTest
+{
+    [TestFixture]
+    class EloadasokTest
+    {
+        public Eloadas test = new Eloadas(4, 2);
+        [TestCase]
+        public void Test1()
+        {
+            Assert.IsTrue(test.Lefoglal());
+        }
+
+    }
+}
